866e333fd45534699f1f752b89ba898307ab6ca8
Ice-Citron <shng2025@gmail.com>
2025-11-25 18:21:17 +0000

    updating progress again so far

---
+++ Copied/SQL-Practice/25-Nov.sql
@@ -0,0 +1,22 @@
+INSERT INTO account
+(126, 'buisness', 'McBrien, P.', 1.00, 67)
+
+------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------
+
+CROSS JOIN 
+INNER JOIN ON theta
+NATURAL JOIN
+USING(k)
+
+LEFT OUTER JOIN
+RIGHT OUTER JOIN
+FULL OUTER JOIN
+
+LEFT SEMI JOIN
+RIGHT SEMI JOIN
+LEFT ANTI JOIN
+RIGHT ANTI JOIN
+
+SELF JOIN
+
+------ ------ ------ ------ ------ ------ ------ ------ ------ ------ ------
