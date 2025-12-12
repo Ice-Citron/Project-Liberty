41f991e5ba621ce1e1dc099adbab3038d1bf3e52
Ice-Citron <shng2025@gmail.com>
2025-12-12 17:24:38 +0000

    wasted webinar

---
+++ Copied/Nvidia-Webinars/Dec-12.cpp
@@ -0,0 +1,21 @@
+/*
+    - Simulation / digital twin -- omniverse / cosmos
+    - Deployment / Run-time computer -- AGX Thor
+    - Training AI Models / Fine-tuning -- DGX
+
+===
+
+// Robotics Demand Rich Sensors (sensor fusion) and Low-Latency AI processing
+
+- High level reasoning (1-5 Hz)
+- Perception and Planning (30 Hz)
+- Real-Time control framework (100 Hz - 1000 Hz)
+- Hardware Abstraction Layer
+
+
+===
+
+// Nvidia Jetson Thor specialties
+    - New Transformer engine architecture
+    - Multi-instnace GPU (MIG) support
+*/
