33849ac12de3b29b6613dabc5f19e02d25eaba84
Ice-Citron <shng2025@gmail.com>
2026-08-22 23:14:50 +0100

    [LADDER]: Conducted additional mathematical learning of quaternions, SLeRP, and reverse engineered and digested CheatCode.py. Will work on my own MyCheatCode.py next.

---
+++ Copied/Nvidia-Issac-Lab/August-15/numpy_practice.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+created_array = np.asarray([0, 0, 0], dtype=np.float32)
+print(created_array)
+
+arr = np.array([1, 2, 3, 4, 5])
+print(arr)
+
+## # ## # # # # #
+print(f"Created array type: {type(created_array)}")
+print(f"Created array type: {type(arr)}")
